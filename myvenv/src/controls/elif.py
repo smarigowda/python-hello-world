8711bb04deec1b266fd76de9795a5b4a70c6f541
Santosh Marigowda <santosh.marigowda@abcam.com>
2022-02-21 07:30:34 +0000

    one more level

--- myvenv/src/controls/elif.py
+++ myvenv/src/controls/elif.py
@@ -1,10 +1,14 @@
-today = 'Monday'
-today = 'Sunday'
-today = 'Saturday'
+today = "Monday"
+today = "Sunday"
+# today = "Saturday"
+condition = "Headache"
 
-if today == 'Saturday':
-    print('Party')
-elif today == 'Sunday':
-    print("Recover")
+if today == "Saturday":
+    print("Party")
+elif today == "Sunday":
+    if condition == "Headache":
+        print("Rest and Recover")
+    else:
+        print("Recover")
 else:
     print("Work...work...work...")
