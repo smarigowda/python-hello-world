64509fc4a1cdd8c497cc490b2cba8cd9b9b1b179
Santosh Marigowda <santosh.marigowda@abcam.com>
2022-02-21 07:23:49 +0000

    else if example

---
+++ myvenv/src/controls/elif.py
@@ -0,0 +1,10 @@
+today = 'Monday'
+today = 'Sunday'
+today = 'Saturday'
+
+if today == 'Saturday':
+    print('Party')
+elif today == 'Sunday':
+    print("Recover")
+else:
+    print("Work...work...work...")
